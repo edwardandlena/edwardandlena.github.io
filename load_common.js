683816653b9235d8404ccdcbdfe6272013cd415f
Edward S. Rice <edward.rice@missouri.edu>
2020-04-05 22:15:06 -0700

    Switched to grid layout

--- load_common.js
+++ load_common.js
@@ -1,14 +1,11 @@
 // add the common header in the right place
 fetch('header.html')
     .then(data => data.text())
-    .then(html => document.getElementById('header').innerHTML = html);
+    .then(function(html) {
+        document.querySelector('header').innerHTML = html;
 
-// add the navbar and highlight the link to the current page
-fetch('nav.html')
-    .then(data => data.text())
-    .then(html => {
-        document.getElementById('navbar').innerHTML = html;
-        document.querySelectorAll('#navbar > a').forEach(navlink => {
+        // highlight the link to the current page
+        document.querySelectorAll('nav a').forEach(navlink => {
             if (navlink.href == window.location.href)
                 navlink.classList.add('current');
         });
