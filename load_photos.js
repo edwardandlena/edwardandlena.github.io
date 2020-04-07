9ce1780f655357470c1a70bd23ece3d7dd112644
Edward S. Rice <edward.rice@missouri.edu>
2020-04-07 16:06:39 -0700

    Added photos

--- load_photos.js
+++ load_photos.js
@@ -10,7 +10,7 @@ function flickrPhotoJSONtoURL(photoJson, size) {
 let flickrParams = {
     method: 'flickr.people.getPublicPhotos',
     api_key: '08e77de6f49140e4ffce2f26bdc5f1ab',
-    user_id: '77651700@N06',
+    user_id: '187805122@N05',
     format: 'json',
     nojsoncallback: '1'
 };
@@ -19,10 +19,10 @@ let url = 'https://www.flickr.com/services/rest/?' + queryString;
 fetch(url)
     .then(response => response.json())
     .then(function(json) {
-        let thisPhoto = json.photos.photo[0];
-        console.log(JSON.stringify(thisPhoto));
-        let photoUrl = flickrPhotoJSONtoURL(thisPhoto, 1);
-        let img = document.createElement('img');
-        img.src = photoUrl;
-        document.getElementById('gallery').append(img);
+        for (photo of json.photos.photo) {
+            let photoUrl = flickrPhotoJSONtoURL(photo, 1);
+            let img = document.createElement('img');
+            img.src = photoUrl;
+            document.getElementById('gallery').append(img);
+        }
     })
